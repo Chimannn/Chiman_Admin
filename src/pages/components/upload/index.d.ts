934a05338d5fd3515c3a8b2cca520987fe9f02f6
chiman <1164617068@qq.com>
2025-03-07 10:59:33 +0800

    fix

--- src/pages/components/upload/index.d.ts
+++
@@ -1,1 +0,0 @@
-export default function UploadPage(): import("react/jsx-runtime").JSX.Element;
--- src/pages/components/upload/index.js
+++
@@ -1,7 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import { Row, Col } from "antd";
-import Card from "@/components/card";
-import { FileUploader } from "@/components/upload";
-export default function UploadPage() {
-    return (_jsx(Card, { style: { justifyContent: "center" }, children: _jsx(Row, { children: _jsx(Col, { children: _jsx(FileUploader, {}) }) }) }));
-}
