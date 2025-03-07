cae5bf7973f295ae16020df8e7d002333d58ab88
chiman <1164617068@qq.com>
2025-03-07 22:09:33 +0800

    fix:修改vercel api

--- api/auth/login.ts
+++ api/auth/login.ts
@@ -4,15 +4,8 @@ import { faker } from "@faker-js/faker";
 module.exports = (req: VercelRequest, res: VercelResponse) => {
     const { username, password } = req.body;
     const DEFAULT_USER = {
-        id: "b34719e1-ce46-457e-9575-99505ecee828",
         username: "admin",
-        email: faker.internet.email(),
-        avatar: faker.image.avatarGitHub(),
-        createdAt: faker.date.anytime(),
-        updatedAt: faker.date.recent(),
         password: "admin",
-        role: ADMIN_ROLE,
-        permissions: ADMIN_ROLE.permission,
     };
 
     if (DEFAULT_USER.username !== username || DEFAULT_USER.password !== password) {
