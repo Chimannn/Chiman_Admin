5c40d65e9329bd2beb6bd537bd909b80059a1394
chiman <1164617068@qq.com>
2025-03-07 22:46:12 +0800

    fix:修改vercel api

--- api/upload/chunks/[uploadId].ts
+++ api/upload/chunks/[uploadId].ts
@@ -1,7 +1,7 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
 import { uploads } from "../const";
 module.exports = (req: VercelRequest, res: VercelResponse) => {
-    const { uploadId } = req.body;
+    const { uploadId } = req.query;
     const upload = uploads.get(uploadId);
 
     res.status(200).send(Array.from(upload.chunks));
