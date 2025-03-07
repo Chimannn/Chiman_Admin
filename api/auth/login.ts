519a0f36d1cf353e522ed7cf4e342e86b421d64b
chiman <1164617068@qq.com>
2025-03-07 21:22:05 +0800

    fix:修改vercel.json

--- api/auth/login.ts
+++ api/auth/login.ts
@@ -1,11 +1,10 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
 import { faker } from "@faker-js/faker";
-import { USER_LIST } from "../assets";
 export default function handler(req: VercelRequest, res: VercelResponse) {
     const { username, password } = req.query;
-    const user = USER_LIST.find((user) => user.username == username);
+    const user = { username: "admin", password: "admin" };
 
-    if (!user || user.password !== password) {
+    if (!user || user.username !== username || user.password !== password) {
         return res.status(200).json({
             code: -1,
             message: "Incorrect username or password.",
