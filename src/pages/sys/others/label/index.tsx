d1eb373430410ab770beb76ced92d010b65a2d89
chiman <1164617068@qq.com>
2025-04-30 18:41:07 +0800

    feat：替换有效cdn

--- src/pages/sys/others/label/index.tsx
+++ src/pages/sys/others/label/index.tsx
@@ -1,7 +1,7 @@
 import Card from "@/components/card";
 import * as THREE from "three";
-import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.176.0/examples/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.176.0/examples/jsm/loaders/GLTFLoader.js";
 import { useEffect, useRef } from "react";
 import "./index.scss";
 export default function BlankPage() {
